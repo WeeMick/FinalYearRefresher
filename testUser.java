b21eb9262fbff58c6effbb581bf0d24ee1360cc4
WeeMick <michaelblr5@gmail.com>
2020-09-27 01:29:32 +0100

    printing username

--- testUser.java
+++ testUser.java
@@ -1,8 +1,8 @@
-package main;
-
-public class testUser {
+public class TestUser {
 
   public static void main(String[] args)  {
     User bob = new User("Bob");
+
+    System.out.println("Users name is " + bob.getUsername());
   }
 }
